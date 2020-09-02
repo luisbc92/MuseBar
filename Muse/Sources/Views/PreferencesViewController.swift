3c35e18cfd22ce354ea673b6383dc5435c1b9875
Matan Mashraki <mail@matanmashraki.com>
2020-09-02 15:05:38 +0300

    Added Sparkle

---
+++ Muse/Sources/Views/PreferencesViewController.swift
@@ -0,0 +1,9 @@
+//
+//  PreferencesViewController.swift
+//  Muse Bar
+//
+//  Created by Matan Mashraki on 02/09/2020.
+//  Copyright © 2020 Matan Mashraki. All rights reserved.
+//
+
+import Foundation
